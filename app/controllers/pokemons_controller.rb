789d8c1fd177c2213aee765862a32169aec0f5c3
Elise Fitzgerald <elise.fitzgerald15@gmail.com>
2015-09-26 21:29:44 -0400

    user can add pokemon

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -4,4 +4,25 @@ before_action :authenticate_user!
   def index
     @pokemons = Pokemon.all
   end
+
+  def new
+    @pokemon = Pokemon.new
+  end
+
+  def create
+    @pokemon = Pokemon.new(pokemon_params)
+
+    if @pokemon.save
+      flash[:notice] = "Pokemon successfully added!"
+      redirect_to pokemons_path
+    else
+      flash[:errors] = @pokemon.errors.full_messages.join(". ")
+      render :new
+    end
+  end
+
+  private
+  def pokemon_params
+    params.require(:pokemon).permit(:name, :ability, :poketype, :strength, :age)
+  end
 end
