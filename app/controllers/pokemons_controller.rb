612045db1c1843e39b71d600ba08d05f7c302b0c
Elise Fitzgerald <elise.fitzgerald15@gmail.com>
2015-09-29 08:42:43 -0400

    quick formatting fix

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -17,7 +17,8 @@ before_action :authenticate_user!
 
   def create
     @pokemon = Pokemon.new(pokemon_params)
-
+    @pokemon.user = current_user
+    
     if @pokemon.save
       flash[:notice] = "Pokemon successfully added!"
       redirect_to pokemons_path
@@ -29,6 +30,6 @@ before_action :authenticate_user!
 
   private
   def pokemon_params
-    params.require(:pokemon).permit(:name, :ability, :poketype, :strength, :age)
+    params.require(:pokemon).permit(:name, :ability, :poketype, :strength, :age, :user_id)
   end
 end
