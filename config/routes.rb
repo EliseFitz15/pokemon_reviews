ad1c65aff483186da9bb0e3e4de0515a6add9f66
Elise Fitzgerald <elise.fitzgerald15@gmail.com>
2015-09-29 09:59:43 -0400

    Update home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'homes#index'
+  root 'pokemons#index'
   devise_for :users
   resources :pokemons, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:index, :new, :create]
